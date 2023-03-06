7d7c1df794549214d2eea87e695570f86225352d
Felipe Brayner <125176719+felipebrayne@users.noreply.github.com>
2023-03-05 23:09:52 -0300

    fiz mais exercicios

---
+++ aula11/ex008.js
@@ -0,0 +1,6 @@
+var vel = 10
+console.log(` A velocidade do seu carro é ${vel}Km/h`)
+if(vel > 60){ //condição simples 
+    console.log('Você ultrpassou a velocidade permiida, MULTADO!')
+}
+console.log('Diriga sempre com cinto de segurança!')
