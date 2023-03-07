45c75795d7bd261bbfb1144f8a5ce7d25dfaf423
Felipe Brayner <125176719+felipebrayne@users.noreply.github.com>
2023-03-07 10:58:55 -0300

    fiz um projeto mudando as cores e a imagem de acordo com a hora do dia

---
+++ ex15.js
@@ -0,0 +1,9 @@
+var idade = 90
+console.log(`Você tem ${idade} anos`)
+if(idade < 16){
+    console.log('Você não pode ter carteira de motorista')
+} else if(idade <= 70){
+    console.log('Você ja pode ter carteira de motorista')
+} else{
+    console.log('Recomendamos a você que não dirija')
+}
